44cce4b81eeed135dd1db15f576b78b4af92d9ac
Omg Bebebe <omgbebebe@gmail.com>
2022-11-28 02:02:52 +0400

    initial gui libs

--- flake.nix
+++ flake.nix
@@ -20,11 +20,26 @@
           # hlsCheck.enable = true;
           haskellPackages = pkgs.haskell.packages.ghc943.override {
             overrides = hsPkgs-old: hsPkgs-new: {
-#              sdl2 = pkgs.haskell.lib.dontCheck (hsPkgs-new.callHackageDirect {
-#                pkg = "sdl2";
-#                ver = "2.5.4.0";
-#                sha256 = "tG1mgGqyn3heGk5jt6V3jXrxvXtXaZh4nKLwlyZwnCQ=";
-#              } {});
+              monomer = hsPkgs-new.callHackageDirect {
+                pkg = "monomer";
+                ver = "1.5.0.0";
+                sha256 = "87by+Safw9NvkRk2RqLr76a7jmVsTg+I2njExV0g7ck=";
+              } {};
+              sdl2 = pkgs.haskell.lib.dontCheck (hsPkgs-new.callHackageDirect {
+                pkg = "sdl2";
+                ver = "2.5.4.0";
+                sha256 = "tG1mgGqyn3heGk5jt6V3jXrxvXtXaZh4nKLwlyZwnCQ=";
+              } {});
+              double-conversion = pkgs.haskell.lib.markUnbroken (pkgs.haskell.lib.overrideSrc hsPkgs-new.double-conversion {
+                src = pkgs.fetchFromGitHub {
+                  owner = "haskell";
+                  repo = "double-conversion";
+                  rev = "5d092e0664442eaac8ae1d101dba57ce9b1c9b03";
+                  sha256 = "K+UJK5ek5ufplqY7to9lfVnSKoa+rZUjNL95b0P7R3w=";
+                };
+              });
+              OpenGLRaw = pkgs.haskell.lib.doJailbreak hsPkgs-new.OpenGLRaw;
+              nanovg = pkgs.haskell.lib.doJailbreak hsPkgs-new.nanovg;
               ListLike = pkgs.haskell.lib.dontCheck hsPkgs-new.ListLike;
               hspec-contrib = hsPkgs-new.callHackageDirect {
                 pkg = "hspec-contrib";
---
+++ src/Gui.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Gui where
+
+import Control.Lens
+import Data.Text (Text)
+import Monomer
+import TextShow
+
+import qualified Monomer.Lens as L
+
+newtype AppModel = AppModel {
+  _clickCount :: Int
+} deriving (Eq, Show)
+
+data AppEvent
+  = AppInit
+  | AppIncrease
+  deriving (Eq, Show)
+
+makeLenses 'AppModel
+
+buildUI
+  :: WidgetEnv AppModel AppEvent
+  -> AppModel
+  -> WidgetNode AppModel AppEvent
+buildUI wenv model = widgetTree where
+  widgetTree = vstack [
+      label "Hello world",
+      spacer,
+      hstack [
+        label $ "Click count: " <> showt (model ^. clickCount),
+        spacer,
+        button "Increase count" AppIncrease
+      ]
+    ] `styleBasic` [padding 10]
+
+handleEvent
+  :: WidgetEnv AppModel AppEvent
+  -> WidgetNode AppModel AppEvent
+  -> AppModel
+  -> AppEvent
+  -> [AppEventResponse AppModel AppEvent]
+handleEvent wenv node model evt = case evt of
+  AppInit -> []
+  AppIncrease -> [Model (model & clickCount +~ 1)]
+
+
+start :: IO ()
+start = do
+  startApp model handleEvent buildUI config
+  where
+    config = [
+      appWindowTitle "Omg Parser",
+      appWindowIcon "./assets/images/icon.png",
+      appTheme darkTheme,
+      appFontDef "Regular" "./assets/fonts/Roboto-Regular.ttf",
+      appInitEvent AppInit
+      ]
+    model = AppModel 0
